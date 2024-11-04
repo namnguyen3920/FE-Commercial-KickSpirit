b5ca15838693cb2b9a3f637182e19526230dc969
HoangNam-WE18013 <ititwe18013.student@gmail.com>
2024-11-04 18:52:32 +0700

    initial push

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'main-color': '#0ea5e9',
+      'header-color' : '#a1a1aa'
+    }
+  },
+  plugins: [
+    
+    ],
+}
+
